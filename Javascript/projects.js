0b1323c3fdac8a78edf405dc7ece6b231cfa687c
lixuan <lilx@unc.edu>
2020-06-11 20:59:41 -0400

    updates to projects

--- Javascript/projects.js
+++ Javascript/projects.js
@@ -1,7 +1,6 @@
 
 
 export const renderCard = function(project) {
-  
     return `
             <div class="card bg-transparent" >
             <img class="card-img-top" src="${project.image}" alt="Card image cap" style="width: 100%;">
@@ -15,20 +14,26 @@ export const renderCard = function(project) {
 };
 
 export const gitCard = function(project) {
-
     return `
-    
-    
-    
-    
-    
+        <div class="card border-0" style="background-color:#151528">
+            <div class="card-body horizontal-center vertical-center">
+                <h5 class="paragraphs"></h5>
+                <a href="#" class="btn btn-primary">Check out my github!</a>     
+            </div>
+        </div>
+    `
+}
+
+export const blankCard = function() {
+    return `
+        <div class="card border-0" style="background-color:#151528">
+        </div>
     `
 }
 
 
 export const addRow = function() {
     return `<div class = "row mb-3">`
-    
 }
 
 
@@ -44,9 +49,13 @@ export const loadProjectsIntoDOM = function(project) {
          cards1.append(renderCard(project[i]));
      }
      let cards2 = $(addRow());
-      for (let i = 3; i < 6; i++) {
+      for (let i = 3; i < 4; i++) {
           cards2.append(renderCard(project[i]));
      }
+     cards2.append(gitCard(project[4]));
+     cards2.append(blankCard());
+
+
 
     // Append cards to the $root element
     $root.addClass().append(cards1);
